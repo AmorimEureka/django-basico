eb0e0e3304e4ced619d3cf298e5bf8bdad5bd8f1
amorimeureka <raffaekk@gmail.com>
2024-02-03 16:09:07 -0300

    feat:Função def no model Cliente para aparecer o nome dos Clientes na área admin

--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    if str(request.user) == 'rafaelamorim':
+        var = f'Seja Bem-Vindo {request.user}'
+    else:
+        var = 'Usuário não está logado'
+    
+    context={'logado':var}
+
+    return render(request, 'index.html', context)
 
 
 def contato(request):
