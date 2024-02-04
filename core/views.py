d219f54f67ecda8cd5eb3ab1010ee023e9f5baa6
amorimeureka <raffaekk@gmail.com>
2024-02-03 21:29:27 -0300

    feat:Retornando dado(s) do db no html

--- core/views.py
+++ core/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 
+from .models import Produto
+
 def index(request):
+    produtos = Produto.objects.all()
+
     if str(request.user) == 'rafaelamorim':
         var = f'Seja Bem-Vindo {request.user}'
     else:
         var = 'Usuário não está logado'
     
-    context={'logado':var}
+    context={
+        'logado':var,
+        'produtos': produtos}
 
     return render(request, 'index.html', context)
 
 
 def contato(request):
     return render(request,'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    contex = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', contex)
